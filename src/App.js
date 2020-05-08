66f326a2020e0638c4d69b98d52ffe2023ad068c
Daniela <dd.jj.rr@me.com>
2020-05-07 19:53:26 -0500

    Tarea Lista

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link, Switch, Redirect} from 'react-router-dom';
+import Now from './Pages/Now';
+import Popular from './Pages/Popular';
+import Top from './Pages/Top';
+import Upcoming from './Pages/Upcoming';
 import './App.css';
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+
+function Navbar() {
+  return (
+    <div className="fija">
+      <Link to="/">Home | </Link>
+      <Link to="/now">Now | </Link>
+      <Link to="/popular">Popular | </Link>
+      <Link to="/top">Top | </Link>
+      <Link to="/upcoming">Upcoming </Link>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <Router>
+          <Switch>
+            <Route exact path="/"> <Home /> </Route>
+            <Route exact path="/now"><Now /> </Route>
+            <Route exact path="/popular"><Popular /> </Route>
+            <Route exact path="/top"><Top /> </Route>
+            <Route exact path="/upcoming"><Upcoming /></Route>
+            <Redirect from="*" to="/" />
+          </Switch>
+          <Navbar />
+        </Router>
+
+
       </header>
     </div>
   );
---
+++ src/Pages/Popular.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import axios from 'axios';
+
+
+class Now extends React.Component {
+    constructor(){
+        super();
+
+        this.state = {
+            loading: true,
+            error: false,
+            list:[]
+        }
+
+    }
+
+
+
+    componentDidMount(){
+        axios.get("https://api.themoviedb.org/3/movie/popular?api_key=435c541ff77bb63474aeac62867098ad&language=en-US&page=1")
+            .then(res => {
+                this.setState({
+                    list: res.data
+                } )
+            } )
+            .catch(e => {
+                this.setState({
+                    error: true
+                } )
+                console.log(e);
+            } )
+            .finally( () =>{
+                this.setState({
+                    loading: false,
+                })
+                console.log(this.state.list);
+
+            } )
+
+
+    }
+
+
+    render(){
+        if (this.state.loading ===true ){ return <h2> Loading...</h2>}
+        if  (this.state.error ===true ){ return <h2> ERROR!!...</h2>}
+
+        return(
+            <table>
+                <tbody>
+            { this.state.list.results.map ( item =>
+                <tr><td><h4>{item.title} ({item.release_date}) </h4> <p>{item.overview} </p> </td></tr>
+                )
+            }
+                </tbody>
+            </table>
+        )
+
+
+
+
+
+    };
+
+}
+
+export default Now;
